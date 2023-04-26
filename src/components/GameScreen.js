05cbd1d5d31f65107da8cd825d190a7402ee3dd9
sertman1 <sertman1@jhu.edu>
2023-04-26 19:23:54 -0400

    started game screen

---
+++ src/components/GameScreen.js
@@ -0,0 +1,38 @@
+const vocabulary = {
+  "Une bagnole" :  "l'Automobile",
+  "Une caisse" : "l'Automobile",
+  "Le bahut": "le lycée",
+  "une baraque": "une maison",
+  "une boîte": "une entreprise",
+  "la bouffe": "les aliments",
+  "un boulot": ("un emploi", "un travail"),
+  "le canard": "le journal",
+  "les chiottes": "les toilettes",
+  "un(e) clope": "une cigarette",
+  "La douloureuse": ("l’addition", "la facture"),
+  "De la flotte": "de l'eau",
+  "Un flic": "un policier",
+  "un foutoir": "un chaos",
+  "un bordel": "un chaos",
+  "un frangin": "un frère",
+  "Des fringues": "des vêtements",
+  "Du fric": "de l'argent",
+  "de la thune": "de l'argent",
+  "du blé": "de l'argent",
+  "Du frometon": "du fromage",
+  "Un gosse": "un enfant",
+  "un môme": "un enfant",
+  "un gamin": "un enfant",
+  "un bambin": "un enfant",
+  "La gratte": "la guitare",
+  "L’hosto": "l’hôpital",
+  "Un machin": ("une chose", "un objet"),
+}
+
+function GameScreen(props) {
+  return (
+    <h1>FRENCH</h1>
+  );
+}
+
+export default GameScreen
