05d5b58dca37debed5211e07821adab0fe62d7d5
sertman1 <sertman1@jhu.edu>
2023-04-26 23:02:25 -0400

    made working starting page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React, { useState } from "react"
 import GameScreen from "./components/GameScreen";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 function App() {
   
+  const [gameStarted, setGameStarted] = useState(false)
+
+  const getDisplay = () => {
+    if (!gameStarted) {
+      return (<Button variant="contained" onClick={() => setGameStarted(true)}>JOUER !</Button>)
+    } else {
+      return (
+        <Box sx={{ p: 2, border: 1, borderColor: 'primary.main', bgcolor: 'secondary.light' }}>
+          <GameScreen></GameScreen>
+        </Box> 
+      )
+    }
+  }
+
+  const showShit = () => {
+    return <p>Hello</p>
+  }
+
   return (
     <div>
       <h1>LE FRANCAIS</h1>
-      <Box sx={{p: 2, border: 1, borderColor: 'primary.main', bgcolor: 'secondary.light'}}>
-        <GameScreen>
-        </GameScreen>
-      </Box> 
+      {getDisplay()}
     </div>
   );
 
